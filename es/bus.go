fe267242af9e25e5c244be8aee9ac5d8580646be
Chris Kolenko <chris.kolenko@compareasiagroup.com>
2021-01-26 23:37:47 +1100

    Implement AddSaga

--- es/bus.go
+++ es/bus.go
@@ -3,12 +3,21 @@ package es
 type Bus interface {
 	CommandBus
 	EventBus
+
+	AddSaga(saga Saga, events ...interface{})
 }
 
 type bus struct {
 	CommandBus
 	EventBus
 }
+
+func (b *bus) AddSaga(saga Saga, events ...interface{}) {
+	handler := NewSagaHandler(b, saga)
+	b.AddHandler(handler, MatchAnyEventOf(events...))
+}
+
+// bus.AddSaga(registerSaga, &eventdata.BankAccountRegistered{})
 
 func NewBus() Bus {
 	commandBus := NewCommandBus()
