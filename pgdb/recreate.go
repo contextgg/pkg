b1b4c6d008d7daea0b9bc6a83a1999754cd65da4
Chris Kolenko <chris@webcanvas.com.au>
2021-09-04 22:57:59 +1000

    Fix up recreate

--- pgdb/recreate.go
+++ pgdb/recreate.go
@@ -1,5 +1,7 @@
 package pgdb
 
+import "fmt"
+
 func Recreate(conn, dbName, dbUser, dbPassword string, opts ...PostgresOptions) (DB, error) {
 	// connect to the postgres db!
 	db, err := SetupPostgres(conn, "postgres", dbUser, dbPassword, opts...)
@@ -28,15 +30,20 @@ func dropConnections(db DB, name string) error {
 	query := `
 		select pg_terminate_backend(pg_stat_activity.pid)
 		from pg_stat_activity
-		where pg_stat_activity.datname = $1 and pid <> pg_backend_pid()`
+		where pg_stat_activity.datname = ? and pid <> pg_backend_pid()`
 	_, err := db.Exec(query, name)
 	return err
 }
 
 func dropCreate(db DB, name string) error {
-	query := `
-    drop database if exists $1; create database $1
-  `
-	_, err := db.Exec(query, name)
-	return err
+	q1 := fmt.Sprintf(`drop database if exists %s`, name)
+	q2 := fmt.Sprintf(`create database %s`, name)
+
+	if _, err := db.Exec(q1); err != nil {
+		return err
+	}
+	if _, err := db.Exec(q2); err != nil {
+		return err
+	}
+	return nil
 }
