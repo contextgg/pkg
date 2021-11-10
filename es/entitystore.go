73d8e3dab1185f7aad625d875dda2e424cb7f674
Chris Kolenko <chris@webcanvas.com.au>
2021-11-11 10:18:59 +1100

    Save more than one entity at a time

--- es/entitystore.go
+++ es/entitystore.go
@@ -1,10 +1,15 @@
 package es
 
-import "context"
+import (
+	"context"
+
+	"github.com/contextgg/pkg/events"
+)
 
 type EntityStore interface {
+	New(ctx context.Context, entityType string, entityId string) (Entity, error)
 	Load(ctx context.Context, entityType string, entityId string, opts ...DataLoadOption) (Entity, error)
-	Save(ctx context.Context, entity Entity) error
+	Save(ctx context.Context, entities ...Entity) error
 }
 
 type entityStore struct {
@@ -12,6 +17,16 @@ type entityStore struct {
 	eventHandler EventHandler
 }
 
+func (e *entityStore) New(ctx context.Context, entityType string, entityId string) (Entity, error) {
+	entityOptions, err := e.EntityRegistry.GetOptions(entityType)
+	if err != nil {
+		return nil, err
+	}
+
+	// make the aggregate
+	entity := entityOptions.Factory(entityId)
+	return entity, nil
+}
 func (e *entityStore) Load(ctx context.Context, entityType string, entityId string, opts ...DataLoadOption) (Entity, error) {
 	entityOptions, err := e.EntityRegistry.GetOptions(entityType)
 	if err != nil {
@@ -26,21 +41,24 @@ func (e *entityStore) Load(ctx context.Context, entityType string, entityId stri
 	store := NewDataStore(unit.Data(), &entityOptions)
 	return store.Load(ctx, entityId)
 }
-func (e *entityStore) Save(ctx context.Context, entity Entity) error {
-	entityOptions, err := e.EntityRegistry.GetOptions(entity.GetTypeName())
-	if err != nil {
-		return err
-	}
-
+func (e *entityStore) Save(ctx context.Context, entities ...Entity) error {
 	unit, err := GetUnit(ctx)
 	if err != nil {
 		return err
 	}
 
-	store := NewDataStore(unit.Data(), &entityOptions)
-	events, err := store.Save(ctx, entity)
-	if err != nil {
-		return err
+	var events []events.Event
+	for _, entity := range entities {
+		entityOptions, err := e.EntityRegistry.GetOptions(entity.GetTypeName())
+		if err != nil {
+			return err
+		}
+		store := NewDataStore(unit.Data(), &entityOptions)
+		out, err := store.Save(ctx, entity)
+		if err != nil {
+			return err
+		}
+		events = append(events, out...)
 	}
 
 	unit.StoreEvents(events...)
@@ -51,7 +69,6 @@ func (e *entityStore) Save(ctx context.Context, entity Entity) error {
 			return err
 		}
 	}
-
 	return nil
 }
 
