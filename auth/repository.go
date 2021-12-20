241c1fece8e1d7a8c8034f2eff35c325fe227417
Chris Kolenko <chris@webcanvas.com.au>
2021-12-21 00:06:18 +1100

    Make auth easier

--- auth/repository.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-import "context"
-
-type Query interface {
-	GetById(ctx context.Context, id string) (*InternalUser, error)
-	GetByUsername(ctx context.Context, username string) (*InternalUser, error)
-}
