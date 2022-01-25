bedf9d981959ed48d0a99b992392601fbd9d326e
Chris Kolenko <chris@webcanvas.com.au>
2022-01-26 02:08:01 +1100

    Set namespace from a header

--- ns/middleware.go
+++ ns/middleware.go
@@ -4,13 +4,29 @@ import (
 	"net/http"
 )
 
-func Middleware(domains ...string) func(next http.Handler) http.Handler {
+func MiddlewareSubdomain(domains ...string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			slug := Slug(r, domains...)
 
 			ctx := r.Context()
 			ctx = SetNamespace(ctx, slug)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		}
+		return http.HandlerFunc(fn)
+	}
+}
+
+func MiddlewareHeader() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+
+			namespace := r.Header.Get("X-Namespace")
+			if len(namespace) > 0 {
+				ctx = SetNamespace(ctx, namespace)
+			}
+
 			next.ServeHTTP(w, r.WithContext(ctx))
 		}
 		return http.HandlerFunc(fn)
