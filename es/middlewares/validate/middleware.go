32fef3f223704f5b0ea5d5166f64ca0fe82e9f44
Chris Kolenko <chris@webcanvas.com.au>
2022-03-13 17:56:38 +1100

    Pass in validator

--- es/middlewares/validate/middleware.go
+++ es/middlewares/validate/middleware.go
@@ -11,12 +11,15 @@ import (
 // NewMiddleware returns a new middleware that validate commands with its own
 // validation method; `Validate() error`. Commands without the validate method
 // will not be validated.
-func NewMiddleware() es.CommandHandlerMiddleware {
-	validate := validator.New()
+func NewMiddleware(validate *validator.Validate) es.CommandHandlerMiddleware {
+	v := validate
+	if v == nil {
+		v = validator.New()
+	}
 
 	return es.CommandHandlerMiddleware(func(h es.CommandHandler) es.CommandHandler {
 		return es.CommandHandlerFunc(func(ctx context.Context, cmd es.Command) error {
-			if err := validate.StructCtx(ctx, cmd); err != nil {
+			if err := v.StructCtx(ctx, cmd); err != nil {
 				return err
 			}
 			// Immediate command execution.
